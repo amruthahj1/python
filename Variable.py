ce887fcef557d0cfcc1ed34292804c7c356455c4
amruthahj1 <47314834+amruthahj1@users.noreply.github.com>
2019-02-18 21:09:11 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,8 @@
+Python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing. Every value in Python has a datatype. 
+Different data types in Python are Numbers, List, Tuple, Strings, Dictionary, etc.
+
+
+25
+/ --> Floating point division
+
+// --> Floor division
