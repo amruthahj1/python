a6629fc8093a53e5dad90d5418858b0a45033fd4
amruthahj1 <47314834+amruthahj1@users.noreply.github.com>
2019-02-18 22:22:43 +0530

    Create Values.py

---
+++ Values.py
@@ -0,0 +1,10 @@
+value is one of the fundamental things — like a word or a number — that a program manipulates. T
+he values we have seen so far are 5 (the result when we added 2 + 3), and "Hello, World!". 
+We often refer to these values as objects and we will use the words value and object interchangeably.
+These objects are classified into different classes, or data types: 4 is an integer, and "Hello, World!" is a string, 
+so-called because it contains a string or sequence of letters. You (and the interpreter) can identify strings because they are enclosed in quotation marks.
+Objects are also called data structures. Python comes with some built-in objects. Some are used so often that Python has a quick way to make these objects, called literals. Th
+e literals include the string, unicode string, integer, float, long, list, tuple and dictionary types.
+
+Python Statement. Instructions that a Python interpreter can execute are called statements.
+For example, a = 1 is an assignment statement. if statement, for statement, while statement etc. 
