7514b5ffa104015cad250f69e2d5cf54d1c02410
amruthahj1 <47314834+amruthahj1@users.noreply.github.com>
2019-02-18 21:14:09 +0530

    Create Class test.py

---
+++ Class test.py
@@ -0,0 +1,9 @@
+BASIS FOR COMPARISON	COMPILER	INTERPRETER
+Input	It takes an entire program at a time.
+It takes a single line of code or instruction at a time.
+Output	It generates intermediate object code.	It does not produce any intermediate object code.
+Working mechanism
+The compilation is done before execution.	Compilation and execution take place simultaneously.
+Speed
+
+Comparatively faster	Slower
