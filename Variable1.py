33b1b6686d9cd328e1fbc3262ea09c326bfd130d
amruthahj1 <47314834+amruthahj1@users.noreply.github.com>
2019-02-18 22:09:45 +0530

    Update and rename Variable.py to Variable1.py

---
+++ Variable1.py
@@ -0,0 +1,6 @@
+Python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing. E
+
+very value in Python has a datatype. Different data types in Python are Numbers, List, Tuple, Strings, Dictionary, etc.
+scope-Global and Local Variables in Python.
+
+Global variables are the one that are defined and declared outside a function and we need to use them inside a function. If a variable with same name is defined inside the scope of function as well then it will print the value given inside the function only and not the global value ...
