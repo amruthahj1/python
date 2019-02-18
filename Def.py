a581c35430dafca997747ddb36aad73e609f55e4
amruthahj1 <47314834+amruthahj1@users.noreply.github.com>
2019-02-18 22:30:49 +0530

    Create Def.py

---
+++ Def.py
@@ -0,0 +1,10 @@
+tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists. The differences between tuples and lists are, the tuples cannot be changed unlike lists and tuples use parentheses, whereas lists use square brackets. C
+reating a tuple is as simple as putting different comma-separated values.
+
+Defining a Function. You can define functions to provide the required functionality.
+Here are simple rules to define a function in Python. Function blocks begin with the keyword def followed by the function name and parentheses ( ( ) ). 
+Any input parameters or arguments should be placed within these parentheses.
+
+Python Lists. List is a collection whic
+h is ordered and changeable. Allows duplicat
+e members. Tuple is a collection which is ordered and unchangeable. Allows duplicate members. Set is a collection which is unordered and unindexed. No duplicate members. Dictionary is a collection which is unordered, changeable
